b758f6f78a4a42e6058bbc0b1def73495a3f36c1
apache-jenkins <apache@httpd.com>
2021-01-11 02:07:00 -0500

    V1

---
+++ apache.sh
@@ -0,0 +1,6 @@
+#!/bash/bash
+echo `yum install httpd -y; systemctl start httpd`
+echo `echo "apache server deployed by jenkins" > /var/www/html/index.html`
+echo "successfully deployed"
+echo "webserver IP is" 
+echo `ip a | grep -i ens33 | awk '{print $2}'`
